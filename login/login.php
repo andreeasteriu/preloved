892f5c99bada853691c7bad73a6b4820e70435cf
decka <decka@github.com>
2019-11-18 17:25:24 +0100

    created login html

---
+++ login/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PRELOVED</title>
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-bg-color">
+        <input type="email" name="email" id="email">
+        <input type="password" name="password" id="password">
+        </div>
+    </div>
+</body>
+</html>
