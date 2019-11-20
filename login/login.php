9fa13bbed3f04f19a0201a5d9ae90d3ed25dea92
decka <decka@github.com>
2019-11-20 17:36:42 +0100

    css to sass login page

--- login/login.php
+++ login/login.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['user'])){
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="login.scss">
+        <link rel="stylesheet" href="login.css">
         <title>PRELOVED</title>
     </head>
     <body>
