d807254c9266f8c6b9526ce0440324d82d011ca4
Ida Holm Green <31645826+Idahgreen@users.noreply.github.com>
2019-11-21 13:33:55 +0100

    Fixes to header.sass & login.sass

--- navigation/header.php
+++ navigation/header.php
@@ -14,54 +14,59 @@
 </head>
 
 <body>
-
-    <header>
-        <a href=""><img src="../graphics/preloved_logo.svg" alt="preloved_logo"></a>
-        <div class="header-login">
-            <a href="">Login</a>
-            <a href="">Sign Up</a>
-        </div>
-    </header>
-    <div class="nav-content">
-        <nav>
-            <div class="dropdown">
-                <h2><a href="">Home</a></h2>
-                <ul>
-                    <h2><a href="">Women</a></h2>
-                    <li><a href="">Coats</a></li>
-                    <li><a href="">Jackets</a></li>
-                    <li><a href="">Blazers</a></li>
-                    <li><a href="">Dresses</a></li>
-                    <li><a href="">Shirts | Tops</a></li>
-                    <li><a href="">Trousers</a></li>
-                    <li><a href="">Jeans</a></li>
-                    <li><a href="">T-Shirts</a></li>
-                    <li><a href="">Sweatshirts</a></li>
-                    <li><a href="">Skirts | Shorts</a></li>
-                    <li><a href="">Shoes</a></li>
-                    <li><a href="">Bags</a></li>
-                </ul>
-
-                <ul>
-                    <h2><a href="">Men</a></h2>
-                    <li><a href="">Coats</a></li>
-                    <li><a href="">Jackets</a></li>
-                    <li><a href="">Blazers</a></li>
-                    <li><a href="">Suits</a></li>
-                    <li><a href="">Shirts</a></li>
-                    <li><a href="">Knitwear</a></li>
-                    <li><a href="">Cashmere</a></li>
-                    <li><a href="">Trousers</a></li>
-                    <li><a href="">Shorts</a></li>
-                    <li><a href="">Jeans</a></li>
-                    <li><a href="">Tracksuits</a></li>
-                    <li><a href="">Sweatshirts</a></li>
-                </ul>
-                <h2><a href="">About</a></h2>
-                <h2><a href="">Contact</a></h2>
+    <div class="header-main">
+        <header>
+            <a href=""><img src="../graphics/preloved_logo.svg" alt="preloved_logo"></a>
+            <div class="header-login">
+                <a href="">Login</a>
+                <a href="">Sign Up</a>
             </div>
-        </nav>
-        <form>
-            <input name="header-search">
-            <button class="header-button"><img src="../graphics/search-bar-icon.svg"></button> </form>
+        </header>
+        <div class="nav-content">
+            <nav>
+                <div class="dropdown">
+                    <h2><a href="">Home</a></h2>
+                    <ul>
+                        <h2><a href="">Women</a></h2>
+                        <div class="header-list-item">
+                            <li><a href="">Coats</a></li>
+                            <li><a href="">Jackets</a></li>
+                            <li><a href="">Blazers</a></li>
+                            <li><a href="">Dresses</a></li>
+                            <li><a href="">Shirts | Tops</a></li>
+                            <li><a href="">Trousers</a></li>
+                            <li><a href="">Jeans</a></li>
+                            <li><a href="">T-Shirts</a></li>
+                            <li><a href="">Sweatshirts</a></li>
+                            <li><a href="">Skirts | Shorts</a></li>
+                            <li><a href="">Shoes</a></li>
+                            <li><a href="">Bags</a></li>
+                        </div>
+                    </ul>
+
+                    <ul>
+                        <h2><a href="">Men</a></h2>
+                        <div class="header-list-item">
+                            <li><a href="">Coats</a></li>
+                            <li><a href="">Jackets</a></li>
+                            <li><a href="">Blazers</a></li>
+                            <li><a href="">Suits</a></li>
+                            <li><a href="">Shirts</a></li>
+                            <li><a href="">Knitwear</a></li>
+                            <li><a href="">Cashmere</a></li>
+                            <li><a href="">Trousers</a></li>
+                            <li><a href="">Shorts</a></li>
+                            <li><a href="">Jeans</a></li>
+                            <li><a href="">Tracksuits</a></li>
+                            <li><a href="">Sweatshirts</a></li>
+                        </div>
+                    </ul>
+                    <h2><a href="">About</a></h2>
+                    <h2><a href="">Contact</a></h2>
+                </div>
+            </nav>
+            <form class="header-form">
+                <input class="header-input">
+                <button class="header-button"><img src="../graphics/search-bar-icon.svg"></button> </form>
+        </div>
     </div>
