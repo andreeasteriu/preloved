f18c98cc69be3de7e54927abdc11765155e65a18
decka <decka@github.com>
2019-11-21 10:01:27 +0100

    Update styling login

--- login/login.php
+++ login/login.php
@@ -1,4 +1,4 @@
-<!-- <?php
+ <!-- <?php
 session_start();
 if(isset($_SESSION['user'])){
     require_once(__DIR__ . '/../login-header.php');
@@ -8,7 +8,7 @@ if(isset($_SESSION['user'])){
     require_once(__DIR__ . '/../navigation/header.php');
 
   }
-?> -->
+?>  -->
 
 
     <!DOCTYPE html>
@@ -21,16 +21,25 @@ if(isset($_SESSION['user'])){
         <title>PRELOVED</title>
     </head>
     <body>
-        
-    <div class="login-container">
+    <div class="login">
+        <div class="login-wrapper">
         <div class="login-bg-color">
-        <h2 class="login-title">
-            Create an Account
-        </h2>
-        <input type="email" name="email" id="email">
-        <input type="password" name="password" id="password">
+        <h3 class="login-title">
+            Login into your account
+        </h3>
+        <div class="login-inputs-grid">
+        <div class="login-inputs">
+        <input type="email" name="email" id="email" placeholder="Email">
+        <input type="password" name="password" id="password" placeholder="Password">
         </div>
+        </div>
+        </div>
+        <button>Login</button>
+    </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     </body>
     </html>
+
+
+<!-- <?php require_once(__DIR__ . '/../footer/footer.php'); ?> -->
