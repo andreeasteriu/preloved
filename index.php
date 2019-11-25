505c3724f09b1f20c09c0d05e094e2a486781e4f
Paulina <paul5177@easv365.dk>
2019-11-25 11:28:27 +0100

    index page added

--- index.php
+++ index.php
@@ -4,19 +4,24 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles/index.sass">
-
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
   <div id="main_picture">
-    <div id='picture'>
-    <div id="title">PRELOVED.</div>
+    <div id='picture' style="background-image: url('pictures/0.png');">
+    <div id="main_title"><span style="color: #95989a;">PRE</span><span style="color: #e8a798;">LOVED.</span></div>
     <div id='title_description'>Another journey chamber way yet females man.</div>
-<div id="row1">
-    <div id="column1">
-      <div id='title1'>BUY</div>
+<div id="main_row">
+    <div id="main_column1">
+      <div id='title1'>     
+        <a href="url"><h1>BUY</h1></a>
+</div>
       <div id='text1'>Another journey chamber way yet females man.</div>
     </div>
-    <div id="column1">
-      <div id='title2'>SELL</div>
+    <div id="main_column2">
+      <div id='title2'> 
+         <a href="url"><h1>SELL</h1></a>
+</div>
       <div id='text2'>Another journey chamber way yet females man.</div>
     </div>
 
@@ -24,62 +29,35 @@
     </div> 
 </div>
 
-<div id='category'>CATEGORIES.</div>
-<div id="category_grid">
-<div class="row">
-  <div class="column" style="background-color:#aaa;">
-    <h2>Column 1</h2>
+<div id='category'><span style="color: #95989a;">CAT</span><span style="color: #e8a798;">EGORIES.</span></div>
+<div class="category_grid">
+  <div class="column" style="background-image: url('pictures/1.png');">
+  <a href="url"><h2>JEANS</h2></a>
   </div>
-  <div class="column" style="background-color:#bbb;">
-    <h2>Column 2</h2>
+  <div class="column" style="background-image: url('pictures/2.png');">
+  <a href="url"><h2>T-SHIRTS</h2></a>
   </div>
-  <div class="column" style="background-color:#bbb;">
-    <h2>Column 3</h2>
+  <div class="column" style="background-image: url('pictures/3.png');">
+  <a href="url"><h2>JACKETS</h2></a>
   </div>
-  <div class="column" style="background-color:#bbb;">
-    <h2>Column 4</h2>
+  <div class="column" style="background-image: url('pictures/4.png');">
+  <a href="url"><h2>SKIRTS</h2></a>
   </div>
+  <div class="column" style="background-image: url('pictures/5.png');">
+  <a href="url"><h2>HOODIES</h2></a>
+  </div>
+  <div class="column" style="background-image: url('pictures/6.png');">
+  <a href="url"><h2>KNITS</h2></a>
+  </div>
+  <div class="column" style="background-image: url('pictures/7.png');">
+  <a href="url"><h2>PANTS</h2></a>
+  </div>
+  <div class="column" style="background-image: url('pictures/8.png');">
+  <a href="url"><h2>DRESSES</h2></a>
 </div>
 
-<div class="row">
-  <div class="column" style="background-color:#ccc;">
-    <h2>Column 5</h2>
-  </div>
-  <div class="column" style="background-color:#ddd;">
-    <h2>Column 6</h2>
-  </div>
-  <div class="column" style="background-color:#ddd;">
-    <h2>Column 7</h2>
-  </div>
-  <div class="column" style="background-color:#ddd;">
-    <h2>Column 8</h2>
-  </div>
 </div>
-</div>
-<script>
 
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("column");
-
-// Declare a loop variable
-var i;
-
-// List View
-function listView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "100%";
-  }
-}
-
-// Grid View
-function gridView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "50%";
-  }
-}
-
-
-</script>
 
 </body>
 </html>
