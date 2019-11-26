48a9c9e8ae74d2e50a2cdf160c2b738ff4b0a244
Paulina <paul5177@easv365.dk>
2019-11-26 11:33:44 +0100

    done-mainpage

--- main/index.php
+++ main/index.php
@@ -19,13 +19,13 @@ if(isset($_SESSION['user'])){
 <div id="main_row">
     <div id="main_column1">
       <div id='title1'>     
-        <a href="url"><h1>BUY</h1></a>
+        <div class="main_description"><a href="url">BUY</a></div>
 </div>
       <div id='text1'>Another journey chamber way yet females man.</div>
     </div>
     <div id="main_column2">
       <div id='title2'> 
-         <a href="url"><h1>SELL</h1></a>
+      <div class="main_description"><a href="url">SELL</a></div>
 </div>
       <div id='text2'>Another journey chamber way yet females man.</div>
     </div>
@@ -37,28 +37,28 @@ if(isset($_SESSION['user'])){
 <div id='category'><span style="color: #e6e6e6;">CAT</span><span style="color: #e8a798;">EGORIES.</span></div>
 <div class="category_grid">
   <div class="column" style="background-image: url('../pictures/1.png');">
-  <a href="url"><h2>JEANS</h2></a>
+  <div class="cat_title"><a href="url">JEANS</a></div>
   </div>
   <div class="column" style="background-image: url('../pictures/2.png');">
-  <a href="url"><h2>T-SHIRTS</h2></a>
+  <div class="cat_title"><a href="url">T-SHIRTS</a></div>
   </div>
   <div class="column" style="background-image: url('../pictures/3.png');">
-  <a href="url"><h2>JACKETS</h2></a>
+  <div class="cat_title"><a href="url">JACKETS</a></div>
   </div>
   <div class="column" style="background-image: url('../pictures/4.png');">
-  <a href="url"><h2>SKIRTS</h2></a>
+  <div class="cat_title"><a href="url">SKIRTS</a></div>
   </div>
   <div class="column" style="background-image: url('../pictures/5.png');">
-  <a href="url"><h2>HOODIES</h2></a>
+  <div class="cat_title"><a href="url">HOODIES</a></div>
   </div>
   <div class="column" style="background-image: url('../pictures/6.png');">
-  <a href="url"><h2>KNITS</h2></a>
+  <div class="cat_title"><a href="url">KNITS</a></div>
   </div>
   <div class="column" style="background-image: url('../pictures/7.png');">
-  <a href="url"><h2>PANTS</h2></a>
+  <div class="cat_title"><a href="url">PANTS</a></div>
   </div>
   <div class="column" style="background-image: url('../pictures/8.png');">
-  <a href="url"><h2>DRESSES</h2></a>
+  <div class="cat_title"><a href="url">DRESSES</a></div>
 </div>
 
 </div>
