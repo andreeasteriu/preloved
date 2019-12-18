121d89898d65205813395d07fca69d7a15aaf0f7
decka <decka@github.com>
2019-12-18 10:12:32 +0100

    done

--- footer/footer.php
+++ footer/footer.php
@@ -1,19 +1,18 @@
 <link rel="stylesheet" href="../footer/footer.css">
 
 <footer id="footer">
-    <div class="logo"><a href=""><img src="../graphics/preloved_logo.svg" alt="preloved_logo"></a>
+    <div class="logo-footer"><a href=""><img src="../graphics/preloved_logo.svg" alt="preloved_logo"></a>
     </div>
-    <div class="links"  style="display: grid;">
+    <div class="links">
     <a href="#">About Us</a>
     <a href="#">Contact</a>
     <a href="#">Terms & Conditions</a>
        
     </div>
-    <div class="social-media" style="display: grid;">
-    <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
-    <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
-    <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
-       
+    <div class="social-media">
+    <a href="#" class="media"><i class="fab fa-instagram"></i><span class="mobile-none-txt"> Instagram</span></a>
+    <a href="#" class="media"><i class="fab fa-twitter"></i> <span class="mobile-none-txt">Twitter</span></a>
+    <a href="#" class="media"><i class="fab fa-facebook-f"></i> <span class="mobile-none-txt">Facebook</span></a>
     </div>
     <div class="newsletter">
         <p>Subscribe to our newsletter!</p>
@@ -23,7 +22,7 @@
         </form>
     </div>
     <div class="address">
-        <div class="ftext">497 Evergreen Rd. Roseville, CA 95673</div>
+        <div class="ftext">497 Evergreen Rd. Roseville</div>
         <div class="ftext">+44 345 678 903</div>
         <div class="ftext">adobexd@mail.com</div>
     </div>
